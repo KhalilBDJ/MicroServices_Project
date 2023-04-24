cd17d0f28c2075a1a1575f42937c74edbc5a5a98
Khalil <khalil.bedjaoui@lojelis.com>
2023-04-24 11:12:05 +0200

    Commande Done

---
+++ Commande/src/main/java/Controllers/CommandeController.java
@@ -0,0 +1,55 @@
+package Controllers;
+
+import Model.Commande;
+import Repositories.CommandeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/commandes")
+public class CommandeController {
+    @Autowired
+    private CommandeRepository commandeRepository;
+
+    @GetMapping
+    public List<Commande> getAllCommandes() {
+        return commandeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Commande> getCommandeById(@PathVariable Long id) {
+        Optional<Commande> commande = commandeRepository.findById(id);
+        if (commande.isPresent()) {
+            return ResponseEntity.ok(commande.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public Commande createCommande(@RequestBody Commande commande) {
+        return commandeRepository.save(commande);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Commande> updateCommande(@PathVariable Long id, @RequestBody Commande updatedCommande) {
+        Optional<Commande> commande = commandeRepository.findById(id);
+        if (commande.isPresent()) {
+            updatedCommande.setId(id);
+            return ResponseEntity.ok(commandeRepository.save(updatedCommande));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCommande(@PathVariable Long id) {
+        commandeRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
