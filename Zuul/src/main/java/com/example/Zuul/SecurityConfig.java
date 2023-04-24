ffc7ae10d8c128cdc368c6ad8ad51141a9052686
Khalil <khalil.bedjaoui@lojelis.com>
2023-04-24 18:07:26 +0200

    Dependencies check

--- Zuul/src/main/java/com/example/Zuul/SecurityConfig.java
+++ Zuul/src/main/java/com/example/Zuul/SecurityConfig.java
@@ -1,4 +1,4 @@
-import org.springframework.context.annotation.Bean;
+/*import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,4 +47,24 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+}*/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().permitAll();
+        http.csrf().disable();
+        return http.build();
+    }
 }
+
